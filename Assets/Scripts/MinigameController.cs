3fc3edd9dc345c31f7b3dfb3f6cdf833fc2bb856
pkrish22 <pranavkkumar0731@gmail.com>
2024-01-26 21:46:16 -0500

    Base for the game manager script along with the minigame controller

---
+++ Assets/Scripts/MinigameController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Normal game logic for the microgame
+    }
+
+    public void GameStart()
+    {
+        // Instantiate the minigame
+    }
+}
