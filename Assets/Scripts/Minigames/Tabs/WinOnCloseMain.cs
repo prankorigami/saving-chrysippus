2c81b5a9e61baa7c1feefde0f046c641bda0d726
IonBingo <grimaceburgman@gmail.com>
2024-01-28 14:26:09 -0500

    WIP Tabs minigame

---
+++ Assets/Scripts/Minigames/Tabs/WinOnCloseMain.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class WinOnCloseMain : MonoBehaviour
+{
+    [SerializeField] private MinigameController controller;
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnMouseClick()
+    {
+        CloseSpam();
+        controller.StopTimer();
+    }
+
+    private void CloseSpam()
+    {
+        anim.Play("closeMain");
+    }
+        
+
+    public void GameEnd()
+    {
+        controller.WinGame();
+    }
+}
---
+++ Assets/Scripts/Minigames/Tabs/WinOnCloseSpam.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class WinOnCloseSpam : MonoBehaviour
+{
+    [SerializeField] private MinigameController controller;
+    Animator anim;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnMouseClick()
+    {
+        CloseSpam();
+        controller.StopTimer();
+    }
+
+    private void CloseSpam()
+    {
+        anim.Play("closeSpam");
+    }
+
+    public void GameEnd()
+    {
+        controller.WinGame();
+    }
+}
