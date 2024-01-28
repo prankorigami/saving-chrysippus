61908b53c749a2e65e08fe25789028e328b7614d
Homshnogen <homshnogen@gmail.com>
2024-01-28 14:12:57 -0500

    he sings

--- Assets/Scripts/Minigames/Comedy Show/ComedyShow.cs
+++ Assets/Scripts/Minigames/Comedy Show/ComedyShow.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class ComedyShow : MinigameController
 {
     [SerializeField] private GameObject[] hands;
+    [SerializeField] private AudioClip jester;
     public int numTomatos = 0;
     private float winTimer = -1f;
     //[SerializeField] private Vector2 xBounds;
@@ -22,6 +24,11 @@ public class ComedyShow : MinigameController
             hands[i].GetComponent<HandClick>().game = this;
             hands[i].transform.localPosition = new Vector3(1.5f*(Random.Range(i+0.1f, i + 0.9f)*2f/hands.Length - 1f), Random.Range(-0.35f, -1.4f), -1f);
         }
+
+        GetComponent<AudioSource>().clip = jester;
+        GetComponent<AudioSource>().time = Random.Range(0f, jester.length);
+        GetComponent<AudioSource>().loop = true;
+        GetComponent<AudioSource>().Play();
     }
 
     // Runs primary game logic, though this can also be deferred to additional scripts.
@@ -42,6 +49,7 @@ public class ComedyShow : MinigameController
         if (numTomatos <= 0) {
             // maybe do an animation, via state and update
             // StopTimer() // @pranav
+            GetComponent<AudioSource>().Stop();
             winTimer = 1f;
         }
     }
